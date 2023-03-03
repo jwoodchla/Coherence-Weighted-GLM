2e59ca10f63b6caf228b41e6b7d2170db9532183
jwoodchla <65416256+jwoodchla@users.noreply.github.com>
2023-03-03 02:35:27 -0800

    Add files via upload

---
+++ robustrange.m
@@ -0,0 +1,23 @@
+function lims = robustrange(s,plims)
+% lims = robustrange(s,plims)
+%
+% Return percentile range of image intensities
+%
+% ARGS :
+% s     = image data
+% plims = lower and upper percentile limits [plow phi]
+%
+% AUTHOR : Mike Tyszka, Ph.D.
+% PLACE  : CHLA Los Angeles, CA
+% DATES  : 07/16/2002 From scratch
+
+% Flatten the image data
+s = s(:);
+n = length(s);
+
+% Sort pixel values
+s = sort(s);
+
+% Calculate nearest indices to percentile limits
+inds = round(plims/100 * n);
+lims = s(inds)';
